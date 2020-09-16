f30c2c46672cd58dd687004d26a5267e71f17eb7
sharon1160 <schullunquia@unsa.edu.pe>
2020-09-16 08:53:12 -0500

    Generator of a vector of random numbers in python

--- Practica01/utils/Exercise_01_2.py
+++ Practica01/utils/Exercise_01_2.py
@@ -5,10 +5,7 @@ import random
 quantity = 1000000
 vector = []
 
-# Numbers within of the range 0f 1 to 1000000
-# vector = [random.randint(1, quantity) for x in range(quantity)] 
-
-# Numbers within of the range 0f 1 to 10
-vector = [random.randint(1, 10) for x in range(quantity)] 
+# Numbers within of the range 0f 1 to 100
+vector = [random.randint(1, 100) for x in range(quantity)] 
 for x in vector:
-    print(x)
+    print(x,end=" ")
