235f6d484959e05111385182f782827bea7c59c8
sharon1160 <schullunquia@unsa.edu.pe>
2020-09-16 08:53:02 -0500

    Generator of a vector of random numbers in c++

--- Practica01/utils/Exercise_01_1.cpp
+++ Practica01/utils/Exercise_01_1.cpp
@@ -4,18 +4,15 @@
 using namespace std;
 
 int main(){
-    
+
     int vector[1000000];
     int quantity = 1000000;
     srand(time(NULL));
 
     for(int i = 0; i < quantity; i++)
     {
-        // -- numbers within the range of 1 to 1000000
-        // vector[i] = 1 + rand() % ((quantity + 1 ) - 1); 
-
-        // -- numbers within the range of de 1 to 10
-        vector[i] = 1 + rand() % (11 - 1); 
+        // numbers within the range of de 1 to 10
+        vector[i] = 1 + rand() % (101 - 1); 
         cout << vector[i] << " ";
     }
 
