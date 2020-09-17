9fb566de9e0d37f03e9a601dc197834730ce7b4f
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-09-17 00:04:37 -0500

    Selection Sort python

--- Practica01/code/SelectionSort/SelectionSort.py
+++ Practica01/code/SelectionSort/SelectionSort.py
@@ -1,7 +1,15 @@
-def SelectionSort(a):
-    for i in range(len(a)):
-        min_idx = i
-        for j in range(i+1, len(a)):
-            if a[min_idx] > a[j]:
-                min_idx = j
-        a[i],a[min_idx] = a[min_idx], a[i]
+def SelectionSort(A,n):
+    for i in range(0,n-1):
+        menor = i
+        for j in range(i+1, n):
+            if A[j] < A[menor]:
+                menor = j
+        temp = A[i]
+        A[i] = A[menor]
+        A[menor] = temp
+
+A = [2,7,4,1,5,3]
+n = 6
+print ("Arreglo Ordenado")
+SelectionSort(A,n)
+print (A)  
