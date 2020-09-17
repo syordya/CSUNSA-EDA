2f82a6c073704c4be93a4711c7d32a540f6f78b9
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-09-17 06:49:30 -0500

    Heap Sort

--- Practica01/code/HeapSort/HeapSort.java
+++ Practica01/code/HeapSort/HeapSort.java
@@ -1,15 +1,15 @@
 import java.io.*;
 
-class HeapSort
+public class HeapSort
 {
    public void sort(int a[],int b)
     {
         int n = b;
 
-        for (int i = n / 2 - 1; i >= 0; i--) heapify(a, n, i);
+        for (int i = n / 2 - 1; i >= 0; i--) 
+        	 heapify(a, n, i);
 
-        for (int i=n-1; i>=0; i--)
-        {
+        for (int i=n-1; i>=0; i--){
             int temp = a[0];
             a[0] = a[i];
             a[i] = temp;
@@ -23,9 +23,11 @@ class HeapSort
         int l = 2*i + 1;
         int r = 2*i + 2;
 
-        if (l < n && a[l] > a[largest]) largest = l;
+        if (l < n && a[l] > a[largest]) 
+        	largest = l;
 
-        if (r < n && a[r] > a[largest]) largest = r;
+        if (r < n && a[r] > a[largest]) 
+        	largest = r;
 
         if (largest != i)
         {
@@ -34,5 +36,18 @@ class HeapSort
             a[largest] = swap;
             heapify(a, n, largest);
         }
+
     }
+    
+	public static void main(String args[]) 
+	{ 
+		HeapSort ob = new HeapSort(); 
+		int A[]={2,7,4,1,5,3};
+		int tam =6;
+		ob.sort(A,tam); 
+		System.out.println("Arreglo Ordenado : "); 
+		for (int i=0; i<tam; ++i) 
+			System.out.print(A[i]+" ");
+		System.out.println();	
+	} 
 }
