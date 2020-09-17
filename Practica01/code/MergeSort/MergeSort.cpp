f48be16f6566f6d242ec844e163a8b3ac8234414
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-09-17 07:37:28 -0500

    Testing with a small size array

--- Practica01/code/MergeSort/MergeSort.cpp
+++ Practica01/code/MergeSort/MergeSort.cpp
@@ -55,3 +55,23 @@ void mergeSort(int a[], int l, int r)
         merge(a, l, m, r);
     }
 }
+
+void printArray(int A[], int size) 
+{ 
+    int i; 
+    for (i = 0; i < size; i++) 
+        printf("%d ", A[i]); 
+    printf("\n"); 
+} 
+  
+int main() 
+{ 
+    int arr[] = { 12, 11, 13, 5, 6, 7 }; 
+    int arr_size = sizeof(arr) / sizeof(arr[0]); 
+  
+    mergeSort(arr, 0, arr_size - 1); 
+  
+    printf("\nSorted array is \n"); 
+    printArray(arr, arr_size); 
+    return 0; 
+} 
