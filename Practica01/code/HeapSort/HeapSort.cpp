a2f751a41f7926d7544be55010b7b0ef0f3812f9
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-09-17 06:36:28 -0500

    Heap Sor c++ probando

--- Practica01/code/HeapSort/HeapSort.cpp
+++ Practica01/code/HeapSort/HeapSort.cpp
@@ -1,35 +1,50 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <iostream> 
 
-void swap(int &a,int &b)
-{
-    int temp = b;
-    b = a;
-    a = temp;
-}
+using namespace std; 
 
-void heapify(int a[], int n, int i)
-{
-    int largest = i;
-    int l = 2*i + 1;
-    int r = 2*i + 2;
-    if (l < n && a[l] > a[largest]) largest = l;
-    if (r < n && a[r] > a[largest]) largest = r;
-    if (largest != i)
-    {
-        swap(a[i], a[largest]);
-        heapify(a, n, largest);
+
+void heapify(int A[], int n, int i){ 
+	int mayor = i;  
+	int l = 2*i + 1; // IZQ = 2*i + 1 
+	int r = 2*i + 2; // DER = 2*i + 2 
+
+	// Si el hijo izquierdo es más grande que la raíz
+	if (l < n && A[l] > A[mayor]) 
+		mayor = l; 
+
+	// Si el hijo derecho es mayor que el mayor hasta ahora
+	if (r < n && A[r] > A[mayor]) 
+		mayor = r; 
+
+	if (mayor != i){ 
+		swap(A[i], A[mayor]); 
+		heapify(A, n, mayor); 
+	} 
+} 
+
+
+void heapSort(int A[], int n) { 
+	//Debe construirse un montIculo inicial
+	for (int i = n / 2 - 1; i >= 0; i--) 
+		heapify(A, n, i); 
+	// Uno por uno extrae un elemento del montón 
+	for (int i=n-1; i>0; i--){ 
+		// Mover la raíz actual al final
+		swap(A[0], A[i]); 
+		// Reconstruir el montículo con los elementos 
+		heapify(A, i, 0); 
+	} 
+} 
+
+
+int main(){ 
+	int A[]={2,7,4,1,5,3};
+	int n = 6; 
+
+	heapSort(A, n); 
+
+	cout << "Arreglo Ordenado \n"; 
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << "\t";
     }
-}
-
-void heapSort(int a[],int b)
-{
-    int n = b;
-    for (int i = n / 2 - 1; i >= 0; i--) heapify(a, n, i);
-    for (int i=n-1; i>=0; i--)
-    {
-        swap(a[0], a[i]);
-        heapify(a, i, 0);
-    }
-}
+} 
