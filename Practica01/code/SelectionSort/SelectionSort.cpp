acc3d318c0fe4e6eccb0e8062edfd0916292d17d
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-09-16 22:52:24 -0500

    Selection Sort en C++ probando con arreglos peques

--- Practica01/code/SelectionSort/SelectionSort.cpp
+++ Practica01/code/SelectionSort/SelectionSort.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void selectionSort(int a[],int b)
-{
-    int min_idx;
-    int n =  b;
-    for (int i = 0; i < n-1; i++)
-    {
-        min_idx = i;
-        for (int j = i+1; j < n; j++)
-        {
-            if (a[j] < a[min_idx]) min_idx = j;
+
+void SelectionSort(int A[],int tam){
+    for (int i = 0; i < tam; i++) {
+        int menor = i;
+        for (int j = i + 1; j < tam; j++) {
+            if (A[j] < A[menor]) {
+            	//Encontrar el N°menor
+                menor = j;     
+            }
         }
-        int temp = a[min_idx];
-        a[min_idx] = a[i];
-        a[i] = temp;
+        //intercambiamos con la posicion i el N°menor
+        int temp = A[i];
+        A[i] = A[menor];
+        A[menor] = temp;
+    }	
+}
+int main() {
+    int A[]={2,7,4,1,5,3};
+    int tam=6;
+    SelectionSort(A,tam);
+    cout << "\nArreglo Ordenado : ";
+    for (int i = 0; i < 6; i++) {
+        cout << A[i] << "\t";
     }
 }
