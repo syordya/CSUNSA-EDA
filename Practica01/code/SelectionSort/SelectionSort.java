d991b5c4ae7875a82fdbf5893e50c830e305fce1
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-09-16 22:50:51 -0500

    Selection Sort arreglo pequeño

--- Practica01/code/SelectionSort/SelectionSort.java
+++ Practica01/code/SelectionSort/SelectionSort.java
@@ -1,25 +1,32 @@
 import java.io.*;
 
-class SelectionSort
-{
-    void sort(int a[],int b)
-    {
-        int n = b;
-        for (int i = 0; i < n-1; i++)
-        {
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-            {
-                if (a[j] < a[min_idx])
-                {
-                    min_idx = j;
-                }
+public class SelectionSort 
+{ 
+	void sort(int A[],int n) 
+	{ 
+		for (int i = 0; i < n-1; i++) 
+		{ 
+			int menor = i; 
+			for (int j = i+1; j < n; j++) 
+				if (A[j] < A[menor]) 
+                    //Encontrar el N°menor
+					menor = j;  
+            //intercambiamos con la posicion i el N°menor
+			int temp = A[menor]; 
+			A[menor] = A[i]; 
+			A[i] = temp; 
+		} 
+	} 
 
-            }
-
-            int temp = a[min_idx];
-            a[min_idx] = a[i];
-            a[i] = temp;
-        }
-    }
-}
+	public static void main(String args[]) 
+	{ 
+		SelectionSort ob = new SelectionSort(); 
+		int A[]={2,7,4,1,5,3};
+		int tam =6;
+		ob.sort(A,tam); 
+		System.out.println("Arreglo Ordenado : "); 
+		for (int i=0; i<tam; ++i) 
+			System.out.print(A[i]+" ");
+		System.out.println();	
+	} 
+} 
