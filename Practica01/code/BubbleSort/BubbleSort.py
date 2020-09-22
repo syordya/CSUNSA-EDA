54f4add338fd23a82cb529064b80358c7662178c
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-09-22 16:42:45 -0500

    Formatting

--- Practica01/code/BubbleSort/BubbleSort.py
+++ Practica01/code/BubbleSort/BubbleSort.py
@@ -6,11 +6,11 @@ def bubbleSort(a):
     n = len(a)
     for i in range(n):
         swapped = False
-        for j in range(0, n-i-1):
-            if a[j] > a[j+1]:
+        for j in range(0, n - i - 1):
+            if a[j] > a[j + 1]:
                 temp = a[j]
-                a[j] = a[j+1]
-                a[j+1] = temp
+                a[j] = a[j + 1]
+                a[j + 1] = temp
                 swapped = True
         if not swapped:
             break
@@ -24,6 +24,6 @@ if __name__ == '__main__':
         bubbleSort(arr)
         end = time.time()
         #print(arr)
-        print(round(end-start, 4), end=' ')
+        print(round(end - start, 4), end=' ')
         n = stdin.readline()
     print()
