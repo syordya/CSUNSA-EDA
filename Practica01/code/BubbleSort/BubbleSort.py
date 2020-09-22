de78b9c35cd4c1ed652dbd7d12d271a18ade1b1e
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-09-22 13:49:54 -0500

    Print execution time of bubbleSort on given arrays

--- Practica01/code/BubbleSort/BubbleSort.py
+++ Practica01/code/BubbleSort/BubbleSort.py
@@ -1,12 +1,29 @@
+from sys import stdin
+import time
+
+
 def bubbleSort(a):
     n = len(a)
     for i in range(n):
         swapped = False
         for j in range(0, n-i-1):
-            if a[j] > a[j+1] :
+            if a[j] > a[j+1]:
                 temp = a[j]
                 a[j] = a[j+1]
                 a[j+1] = temp
                 swapped = True
-        if swapped == False:
+        if not swapped:
             break
+
+
+if __name__ == '__main__':
+    n = stdin.readline()
+    while (n):
+        arr = [int(x) for x in stdin.readline().split()]
+        start = time.time()
+        bubbleSort(arr)
+        end = time.time()
+        #print(arr)
+        print(round(end-start, 4), end=' ')
+        n = stdin.readline()
+    print()
