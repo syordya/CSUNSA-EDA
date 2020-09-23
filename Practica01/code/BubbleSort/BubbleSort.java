15a5ea3e442a1a853f67ee6421392de5029d39de
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-09-22 20:09:54 -0500

    Print execution time on given arrays

--- Practica01/code/BubbleSort/BubbleSort.java
+++ Practica01/code/BubbleSort/BubbleSort.java
@@ -1,27 +1,47 @@
-import java.io.*;
+import java.util.Scanner;
 
-class BubbleSort
-{
-    static void sort(int[] a,int b)
-    {
-        int temp,tamanho;
-        tamanho = b;
-        boolean swapped;
-        for (int i = 0; i < tamanho - 1; i++)
-        {
-            swapped = false;
-            for (int j = 0; j < tamanho - i - 1; j++)
-            {
-                if (a[j] > a[j + 1])
-                {
-                    temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            if (swapped == false)
-                break;
+class BubbleSort {
+  static void sort(int[] a) {
+    int n = a.length;
+    for (int i = 0; i < n; ++i) {
+      boolean swapped = false;
+      for (int j = 0; j < n - i - 1; ++j) {
+        if (a[j] > a[j + 1]) {
+          int temp = a[j];
+          a[j] = a[j + 1];
+          a[j + 1] = temp;
+          swapped = true;
         }
+      }
+      if (!swapped) {
+        break;
+      }
     }
+  }
+
+  static void print(int[] a) {
+    for (int i = 0; i < a.length; ++i) {
+      System.out.print(a[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNextInt()) {
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      for (int i = 0; i < n; ++i) {
+        int x = sc.nextInt();
+        arr[i] = x;
+      }
+      long start = System.nanoTime();
+      sort(arr);
+      long end = System.nanoTime();
+      // print(arr);
+      System.out.print((end - start) / 1000000000.0 + " ");
+    }
+    System.out.println();
+    sc.close();
+  }
 }
