9545a6e3fb0b2b14f4c7d667d3da116dd177397f
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-09-24 12:24:49 -0500

    Renaming folder

---
+++ Practica01/utils/plots/BubbleSort.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+t, a = np.loadtxt('../../data/BubbleSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+x, y = np.loadtxt('../../data/BubbleSort/valores_python.txt',
+                  delimiter=',',
+                  unpack=True)
+m, n = np.loadtxt('../../data/BubbleSort/valores_java.txt',
+                  delimiter=',',
+                  unpack=True)
+plt.plot(t, a, color="blue", label="C++")
+plt.plot(x, y, color="red", label="Python")
+plt.plot(m, n, color="green", label="Java")
+plt.title('BubbleSort')
+plt.xlabel('# elementos')
+plt.ylabel('tiempo-segundos')
+plt.legend()
+plt.show()
---
+++ Practica01/utils/plots/CountingSort.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+t, a = np.loadtxt('../../data/CountingSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+x, y = np.loadtxt('../../data/CountingSort/valores_python.txt',
+                  delimiter=',',
+                  unpack=True)
+m, n = np.loadtxt('../../data/CountingSort/valores_java.txt',
+                  delimiter=',',
+                  unpack=True)
+plt.plot(t, a, color="blue", label="C++")
+plt.plot(x, y, color="red", label="Python")
+plt.plot(m, n, color="green", label="Java")
+plt.title('CountingSort')
+plt.xlabel('# elementos')
+plt.ylabel('tiempo-segundos')
+plt.legend()
+plt.show()
