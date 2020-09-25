4e87b17b1215bcb0517880df53a5f7318684d071
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-09-24 21:30:19 -0500

    Fixing path to data and Renaming

---
+++ Practica01/utils/plots/AllAlgorithmsCpp.py
@@ -0,0 +1,36 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+a, b = np.loadtxt('../../data/BubbleSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+c, d = np.loadtxt('../../data/CountingSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+e, f = np.loadtxt('../../data/HeapSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+g, h = np.loadtxt('../../data/InsertionSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+i, j = np.loadtxt('../../data/MergeSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+k, l = np.loadtxt('../../data/QuickSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+m, n = np.loadtxt('../../data/SelectionSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+plt.plot(a, b, color="blue", label="BubbleSort")
+plt.plot(c, d, color="red", label="CountingSort")
+plt.plot(e, f, color="green", label="HeapSort")
+plt.plot(g, h, color="cyan", label="InsertionSort")
+plt.plot(i, j, color="yellow", label="MergeSort")
+plt.plot(k, l, color="black", label="QuickSort")
+plt.plot(m, n, color="magenta", label="SelectionSort")
+plt.title('Algoritmos Ordenamiento C++')
+plt.xlabel('# elementos')
+plt.ylabel('tiempo-segundos')
+plt.legend()
+plt.show()
---
+++ Practica01/utils/plots/QuickSort.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+t, a = np.loadtxt('../../data/QuickSort/valores_c++.txt',
+                  delimiter=',',
+                  unpack=True)
+x, y = np.loadtxt('../../data/QuickSort/valores_python.txt',
+                  delimiter=',',
+                  unpack=True)
+m, n = np.loadtxt('../../data/QuickSort/valores_java.txt',
+                  delimiter=',',
+                  unpack=True)
+plt.plot(t, a, color="blue", label="C++")
+plt.plot(x, y, color="red", label="Python")
+plt.plot(m, n, color="green", label="Java")
+plt.title('QuickSort')
+plt.xlabel('# elementos')
+plt.ylabel('tiempo-segundos')
+plt.legend()
+plt.show()
