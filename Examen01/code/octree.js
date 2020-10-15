5d9a614a8f17f35bd537aa6b417e80965c78c6ea
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-10-15 06:07:43 -0500

    Merging changes

--- Examen01/code/octree.js
+++ Examen01/code/octree.js
@@ -1,10 +1,10 @@
-var alpha = 0.2;
-
 class Point {
+  //Posicion x , y , z
   constructor(x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
+    //Creamos la esfera
     this.geometry = new THREE.SphereGeometry(2, 10, 10);
     this.material = new THREE.MeshPhongMaterial();
     this.sphere = new THREE.Mesh(this.geometry, this.material);
@@ -13,16 +13,16 @@ class Point {
   }
 }
 
-class Box {
+class Cubo {
   constructor(x, y, z, w, h, d) {
     this.x = x;
     this.y = y;
     this.z = z;
-    this.w = w;
-    this.h = h;
-    this.d = d;
+    this.w = w; //ancho
+    this.h = h; //altura
+    this.d = d;//profundidad
   }
-
+  // verifica si este objeto contiene un objeto Punto
   contains(point) {
     return (point.x >= (this.x - this.w) &&
       point.x <= (this.x + this.w) &&
@@ -32,129 +32,164 @@ class Box {
       point.z <= (this.z + this.d));
   }
 
-  intersects(box) {
-    return !(box.x - box.w > this.x + this.w ||
-      box.x + box.w < this.x - this.w ||
-      box.y - box.h > this.y + this.h ||
-      box.y + box.h < this.y - this.h ||
-      box.z - box.d > this.z + this.d ||
-      box.z + box.d < this.z - this.d);
+  // verifica si este objeto se intersecta con otro objeto Cubo
+  intersects(range) {
+    return !(range.x - range.w > this.x + this.w ||
+      range.x + range.w < this.x - this.w ||
+      range.y - range.h > this.y + this.h ||
+      range.y + range.h < this.y - this.h ||
+      range.z - range.d > this.z + this.d ||
+      range.z + range.d < this.z - this.d);
   }
 }
 
-class Octree {
-  constructor(box, n) {
-    this.box = box;
-    this.capacity = n;
-    this.points = [];
+class OcTree {
+  constructor(cubo, n) {
+    this.cubo = cubo; // cubo
+    this.capacity = n; // capacidad maxima de cada cuadrante
+    this.points = []; // vector , almacena los puntos a almacenar
     this.divided = false;
-
+    //Math.random() retorna un punto flotante, un número pseudo-aleatorio dentro del rango [0, 1).
+    // Separe los valores RGB entre 0 y 1
     this.color = new THREE.Color(Math.random() * 1, Math.random() * 1, Math.random() * 1);
-
-    this.geometry = new THREE.BoxGeometry(box.w * 2, box.h * 2, box.d * 2);
-    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00, opacity: alpha, transparent: true, });
+    //CUBO
+    this.geometry = new THREE.BoxGeometry(cubo.w * 2, cubo.h * 2, cubo.d * 2);
+    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00, opacity: 0.2, transparent: true, });
     this.material.color.set(this.color);
     this.cube = new THREE.Mesh(this.geometry, this.material);
-    this.cube.position.set(box.x, box.y, box.z);
+    this.cube.position.set(cubo.x, cubo.y, cubo.z);
     scene.add(this.cube);
   }
 
-  subdivide() {
-    let x = this.box.x;
-    let y = this.box.y;
-    let z = this.box.z;
-    let w = this.box.w / 2;
-    let h = this.box.h / 2;
-    let d = this.box.d / 2;
-
-    let nof = new Box(x - w, y - h, z - d, w, h, d);
-    let nef = new Box(x + w, y - h, z - d, w, h, d);
-    let sof = new Box(x - w, y + h, z - d, w, h, d);
-    let sef = new Box(x + w, y + h, z - d, w, h, d);
-    let nob = new Box(x - w, y - h, z + d, w, h, d);
-    let neb = new Box(x + w, y - h, z + d, w, h, d);
-    let sob = new Box(x - w, y + h, z + d, w, h, d);
-    let seb = new Box(x + w, y + h, z + d, w, h, d);
-
-    this.sonNOF = new Octree(nof, this.capacity);
-    this.sonNEF = new Octree(nef, this.capacity);
-    this.sonSOF = new Octree(sof, this.capacity);
-    this.sonSEF = new Octree(sef, this.capacity);
-    this.sonNOB = new Octree(nob, this.capacity);
-    this.sonNEB = new Octree(neb, this.capacity);
-    this.sonSOB = new Octree(sob, this.capacity);
-    this.sonSEB = new Octree(seb, this.capacity);
-
-    alpha = alpha + 0.1;
-    this.divided = true;
-  }
-
   insert(point) {
-    if (!this.box.contains(point)) {
+    //Si el punto no esta en los limites (cubo)del octree Return
+    if (!this.cubo.contains(point)) {
       return false;
     }
+    //Si ( this . points . length ) < ( this . capacity )
     if (this.points.length < this.capacity) {
+      //Insertamos en el vector this . points
       this.points.push(point);
+      //Color a la esfera
       point.material.color.set(this.color);
       return true;
-    }
-    if (!this.divided) {
-      this.subdivide();
-    }
-    return (this.sonNOF.insert(point) ||
-      this.sonNEF.insert(point) ||
-      this.sonSOF.insert(point) ||
-      this.sonSEF.insert(point) ||
-      this.sonNOB.insert(point) ||
-      this.sonNEB.insert(point) ||
-      this.sonSOB.insert(point) ||
-      this.sonSEB.insert(point));
-  }
 
-  query(box, found) {
-    if (!this.box.intersects(box)) {
-      return;
     }
     else {
-      for (let p of this.points) {
-        if (box.contains(p)) {
-          p.material.color.set(new THREE.Color(1, 1, 1));
-          found.push(p);
-        }
+      //Dividimos si aun no ha sido dividido
+      if (!this.divided) {
+        this.subdivide();
       }
-      if (this.divided) {
-        this.sonNOF.query(box, found);
-        this.sonNEF.query(box, found);
-        this.sonSOF.query(box, found);
-        this.sonSEF.query(box, found);
-        this.sonNOB.query(box, found);
-        this.sonNEB.query(box, found);
-        this.sonSOB.query(box, found);
-        this.sonSEB.query(box, found);
-      }
+      //Insertamos recursivamente en los 8 hijos
+      this.northeastf.insert(point);
+      this.northwestf.insert(point);
+      this.southeastf.insert(point);
+      this.southwestf.insert(point);
+      this.northeastb.insert(point);
+      this.northwestb.insert(point);
+      this.southeastb.insert(point);
+      this.southwestb.insert(point);
     }
   }
 
-  cleanQuery(box) {
-    if (!this.box.intersects(box)) {
+  //divide el octree en 8 octrees
+  subdivide() {
+    let x = this.cubo.x;
+    let y = this.cubo.y;
+    let z = this.cubo.z;
+    let w = this.cubo.w / 2;
+    let h = this.cubo.h / 2;
+    let d = this.cubo.d / 2;
+
+    // Crear 8 hijos
+    let nef = new Cubo(x + w, y - h, z - d, w, h, d);
+    let nwf = new Cubo(x - w, y - h, z - d, w, h, d);
+    let sef = new Cubo(x + w, y + h, z - d, w, h, d);
+    let swf = new Cubo(x - w, y + h, z - d, w, h, d);
+
+    let neb = new Cubo(x + w, y - h, z + d, w, h, d);
+    let nwb = new Cubo(x - w, y - h, z + d, w, h, d);
+    let seb = new Cubo(x + w, y + h, z + d, w, h, d);
+    let swb = new Cubo(x - w, y + h, z + d, w, h, d);
+
+    //Asignar los OcTree creados a cada hijo
+    this.northeastf = new OcTree(nef, this.capacity);
+    this.northwestf = new OcTree(nwf, this.capacity);
+    this.southeastf = new OcTree(sef, this.capacity);
+    this.southwestf = new OcTree(swf, this.capacity);
+
+    this.northeastb = new OcTree(neb, this.capacity);
+    this.northwestb = new OcTree(nwb, this.capacity);
+    this.southeastb = new OcTree(seb, this.capacity);
+    this.southwestb = new OcTree(swb, this.capacity);
+
+
+    //Hacer : this . divided <- true
+    this.divided = true;
+
+  }
+
+
+  query(range, found) {
+    if (!found) {
+      found = [];
+    }
+    //no se intercepta con los limites del cuadrante
+    if (!range.intersects(this.cubo)) {
+      return found;
+    }
+    //Ciclo por cada punto del ocdtree
+    for (let p of this.points) {
+      //Verificamos si esta dentro del rango
+      if (range.contains(p)) {
+        //1 1 1 blanco / Cambia este color
+        p.material.color.set(new THREE.Color(0, 0, 0));
+        //Lo insertamos en el vector found
+        found.push(p);
+        //count++;
+      }
+    }
+    //Si ha sido dividido
+    if (this.divided) {
+      //Llamamos recursivamente a cada hijo
+      this.northwestf.query(range, found);
+      this.northeastf.query(range, found);
+      this.southwestf.query(range, found);
+      this.southeastf.query(range, found);
+      this.northwestb.query(range, found);
+      this.northeastb.query(range, found);
+      this.southwestb.query(range, found);
+      this.southeastb.query(range, found);
+    }
+    return found;
+  }
+
+  cleanQuery(range) {
+    if (!range.intersects(this.cubo)) {
       return;
     }
     else {
+      //Ciclo por cada punto del ocdtree
       for (let p of this.points) {
-        if (box.contains(p)) {
+        //Verificamos si esta dentro del rango
+        if (range.contains(p)) {
+          //Vuelve a su color
           p.material.color.set(this.color);
         }
       }
+      //Llamamos recursivamente a cada hijo
       if (this.divided) {
-        this.sonNOF.cleanQuery(box);
-        this.sonNEF.cleanQuery(box);
-        this.sonSOF.cleanQuery(box);
-        this.sonSEF.cleanQuery(box);
-        this.sonNOB.cleanQuery(box);
-        this.sonNEB.cleanQuery(box);
-        this.sonSOB.cleanQuery(box);
-        this.sonSEB.cleanQuery(box);
+        this.northwestf.cleanQuery(range);
+        this.northeastf.cleanQuery(range);
+        this.southwestf.cleanQuery(range);
+        this.southeastf.cleanQuery(range);
+        this.northwestb.cleanQuery(range);
+        this.northeastb.cleanQuery(range);
+        this.southwestb.cleanQuery(range);
+        this.southeastb.cleanQuery(range);
       }
     }
   }
+
+
 }
---
+++ Examen01/code/sketch.js
@@ -0,0 +1,48 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+scene.background = new THREE.Color(0xffffff);
+document.body.appendChild(renderer.domElement);
+
+camera.position.y = -20;
+camera.position.x = -100;
+camera.position.z = -100;
+var light = new THREE.DirectionalLight(0xffffff, 0.8);
+light.position.set(-100, 100, 0);
+camera.add(light);
+scene.add(camera);
+
+var axisHelper = new THREE.AxisHelper(1.25);
+scene.add(axisHelper);
+
+var octree;
+var q;
+
+function createOctree(m, n) {
+    let cubo = new Cubo(m / 2, m / 2, m / 2, m / 2, m / 2, m / 2);
+    octree = new OcTree(cubo, n);
+}
+
+
+function rand(n, m) {
+    for (i = 0; i < n; i++) {
+        let x = Math.random() * m;
+        let y = Math.random() * m;
+        let z = Math.random() * m;
+        let point = new Point(x, y, z);
+        octree.insert(point);
+    }
+}
+
+
+controls.update();
+
+var animate = function () {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+};
+
+animate();
