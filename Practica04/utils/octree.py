767d458209d0b815881a3f267aa1b3889255b602
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-10-22 06:31:14 -0500

    Punto 1 y 2

---
+++ Practica04/utils/octree.py
@@ -0,0 +1,39 @@
+class Color(object):
+	def __init__(self, red=0, green=0, blue=0):
+		self.red = red
+		self.green = green
+		self.blue = blue
+
+class NodoOctree(object):
+
+	def __init__(self, nivel,padre):	
+		#El nodo debe almacenar un color en RGB
+		self.color = Color(0,0,0)
+		# un contador (parasaber cuantos pixeles tienen ese color) 
+		self.cont_pixel = 0
+		#un flag para saber si es nodo hoja.
+		self.hoja = false
+		self.hijos=[0 for _ in range(8)]
+	
+	def es_hoja():
+		#saber si es nodo hoja(no tiene hijos)
+		return self.cont_pixel > 0;
+
+	#Lea los pixeles de una imagen
+    #Obtenga una suma de la cantidad de píxeles para el nodo y sus hijos
+	def get_cantPixeles(self):
+		suma = self.contador_pixel
+		for i in range(8):
+			nodo = self.hijos[i]
+			if nodo:
+				suma = suma + nodo.cont_pixel
+		return 
+#Construya el Octree segun el algoritmo explicado en clases.
+class Octree(object):
+
+	def __init__(self, arg):
+		self.niveles={i: [] for i in range(OctreeQuantizer.MAX_DEPTH)}
+		self.root = NodoOctree(0,self)
+
+		
+		
