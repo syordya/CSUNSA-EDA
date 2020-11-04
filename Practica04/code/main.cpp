7f1721de8ad6c787c2326293d7f7d492678ee40f
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-11-04 06:35:06 -0500

    Update main.cpp

--- Practica04/code/main.cpp
+++ Practica04/code/main.cpp
@@ -1,38 +1,40 @@
 #include <opencv2/highgui.hpp>
 
-#include "octree_quantizer.hpp"
+#include "octree_quantization.hpp"
+using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[]) {
-  cv::Mat input, output, out_palette;
+  Mat input, output, out_palette;
 
-  std::string file_name = argv[1];
+  string file_name = argv[1];
+  
+	input = imread(file_name, IMREAD_COLOR);
+	output = imread(file_name, IMREAD_COLOR);
+	out_palette = imread(file_name, IMREAD_COLOR);
 
-  input = cv::imread(file_name, cv::IMREAD_COLOR);
-  output = cv::imread(file_name, cv::IMREAD_COLOR);
-  out_palette = cv::imread(file_name, cv::IMREAD_COLOR);
+	OctreeQuantizer quantizer;
 
-  OctreeQuantizer quantizer;
+	quantizer.fill(output);
+	quantizer.reduction();
+	quantizer.reduction();
+	quantizer.reduction();
+	quantizer.reduction();
+	quantizer.reduction();
+	quantizer.reduction();
+	quantizer.reconstruction(output);
+	quantizer.palette(out_palette);
 
-  quantizer.fill(output);
-  quantizer.reduction();
-  quantizer.reduction();
-  quantizer.reduction();
-  quantizer.reduction();
-  quantizer.reduction();
-  quantizer.reduction();
-  quantizer.reconstruction(output);
-  quantizer.palette(out_palette);
+	namedWindow("Entrada", WINDOW_AUTOSIZE);
+	imshow("Entrada", input);
 
-  cv::namedWindow("Entrada", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Entrada", input);
+	namedWindow("Resultado", WINDOW_AUTOSIZE);
+	imshow("Resultado", output);
 
-  cv::namedWindow("Resultado", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Resultado", output);
+	namedWindow("Paleta de colores",WINDOW_AUTOSIZE);
+	imshow("Paleta de colores", out_palette);
 
-  cv::namedWindow("Paleta de colores", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Paleta de colores", out_palette);
+	waitKey(0);
 
-  cv::waitKey(0);
-
-  return 0;
+	return 0;
 }
