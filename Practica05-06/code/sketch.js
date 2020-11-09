bcd72d943a59a2a2c44c346929e67be8cb5a9c4b
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-11-09 14:30:05 -0500

    Sketch map, given point and closest point

--- Practica05-06/code/sketch.js
+++ Practica05-06/code/sketch.js
@@ -1,7 +1,9 @@
 function setup() {
   var width = 250;
   var height = 200;
-  createCanvas(width, height);
+
+  var canvas = createCanvas(width, height);
+  canvas.parent('parentCanvas');
 
   background(0);
   for (var x = 0; x < width; x += width / 10) {
@@ -13,6 +15,7 @@ function setup() {
     }
   }
 
+  /*
   var data = [];
   for (let i = 0; i < 6; i++) {
     var x = Math.floor(Math.random() * height);
@@ -21,14 +24,52 @@ function setup() {
 
     fill(255, 255, 255);
     circle(x, height - y, 7);
-    textSize(8);
     text(x + ',' + y, x + 5, height - y);
+    textSize(8);
   }
+  */
+
   //var data = [[21,84],[90,102],[5,150],[84,138],[161,29],[181,45],[6,114],[148,85],[106,189],[158,120]]
+  var data = [[100,100],[40,70],[90,40],[70,130],[175,100],[150,30],[140,110]];
+  var point = [140, 90];
+
+  var x =  point[0];
+  var y =  point[1];
+
+  fill(81, 209, 246);
+  circle(x, height - y, 7);
+  textSize(8);
+  text(x + ',' + y, x + 5, height - y);
+
+  data.forEach((point) => {
+    fill(255, 255, 255);
+    circle(point[0], height - point[1], 7);
+    textSize(8);
+    text(point[0] + ',' + point[1], point[0] + 5, height - point[1]);
+  });
 
 
   var root = buildKdTree(data);
   console.log(root);
   var graphDot = "digraph G {\n" + generateDot(root) + '}';
   console.log(graphDot);
+  var best = closestPoint(root, point);
+  console.log(best);
+
+  var x =  best[0];
+  var y =  best[1];
+
+  fill(57, 255, 20);
+  circle(x, height - y, 7);
+  textSize(8);
+  text(x + ',' + y, x + 5, height - y);
+
+  kvecinos = []
+  knn(root, point, kvecinos);
+  console.log(kvecinos)
+
+  var image = Viz(graphDot, {format: 'svg'});
+  var graph = document.getElementById('graphHolder');
+
+  graph.innerHTML = image;		// SVG
 }
