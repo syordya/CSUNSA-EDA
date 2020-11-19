057eebee74a8dcaa160502d8b45a4903a0ba71e6
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-11-19 08:04:29 -0500

    Resize image to 64x64. Add header to csv generated file

--- Examen02/code/src/feature_extractor_one.py
+++ Examen02/code/src/feature_extractor_one.py
@@ -16,18 +16,21 @@ imageFilename = args["image"]
 
 # load the image
 image = cv2.imread(imageFilename)
+# extract class label
+label = imageFilename.split(os.path.sep)[-1].split(".")[0]
 
 # resize the image to a fixed size
-image = cv2.resize(image, (32, 32))
+image = cv2.resize(image, (64, 64))
 
 # feature matrix
-features = []
+features = [["meanB","meanG","meanR","stdevB","stdevG","stdevR", "class"]]
 
 # image descriptor: color mean and standard deviation
 (means, stds) = cv2.meanStdDev(image)
 stats = np.concatenate([means, stds]).flatten()
 
 stats = stats.tolist()
+stats.append(label)
 
 # add features
 features.append(stats)
